cb3149201c395ffbd9868eefd7904505ee4a2db5
Celine <celine.zhang2007@gmail.com>
2025-07-07 16:20:29 -0400

    removed the "battleground"

--- examples/pushmerge_zkp/first_game.py
+++ examples/pushmerge_zkp/first_game.py
@@ -1,4 +1,3 @@
-"""This is the first game we implement."""
 import sys
 import pygame
 from suit_card_graphics import SuitCardGraphics
@@ -43,35 +42,6 @@ manager.add_set(
 
 card_back = AbstractCard("")
 card_back.graphics_type = CardBackGraphics
-# ennemy_cards = CardsSet([card_back, card_back, card_back])
-# ennemy_cards_graphics = AlignedHand(ennemy_cards, card_set_size, card_size=card_size)
-# manager.add_set(
-#     ennemy_cards_graphics,
-#     # Place them in front on the screen
-#     (width / 4, 0),
-#     # Remove the possibility to interact with enemy cards
-#     CardSetRights(
-#         draggable_in=False, draggable_out=False, highlight_hovered_card=False
-#     ),
-# )
-
-battle_ground = CardsSet()
-battle_ground_graphics = AlignedHand(
-    battle_ground,
-    card_size=(1.2 * card_size[0], 1.2 * card_size[1]),
-    size=(1.2 * card_set_size[0], 1.2 * card_set_size[1]),
-    max_cards=2,
-)
-manager.add_set(
-    battle_ground_graphics,
-    (
-        (width - battle_ground_graphics.size[0]) / 2,
-        (height - battle_ground_graphics.size[1]) / 2,
-    ),
-    # Remove the possibility to remove cards
-    CardSetRights(draggable_in=True, draggable_out=False),
-)
-
 
 pygame.display.flip()
 
@@ -79,40 +49,9 @@ clock = pygame.time.Clock()
 
 annimation_tick_left = 0
 
-
-# def enemy_plays_cards():
-#     """Make the enemy play his card"""
-#     card_taken = ennemy_cards_graphics.cardset[0]
-#     ennemy_cards_graphics.remove_card(card_taken)
-#     battle_ground_graphics.append_card(card_taken)
-
-
-# enemy_plays_cards()
-
 while 1: # game loop
     screen.fill("black")
     time_delta = clock.tick(60) / 1000.0
-
-    if annimation_tick_left > 0:
-        # Show a win annimation
-
-        if annimation_tick_left == 1:
-            # Give back the cards from each player
-            opp_card = battle_ground[0]
-            # ennemy_cards_graphics.append_card(opp_card)
-            battle_ground_graphics.remove_card(opp_card)
-
-            # My card was played second
-            my_card = battle_ground[0]
-            my_cards_graphics.append_card(my_card)
-            battle_ground_graphics.remove_card(my_card)
-
-            # enemy_plays_cards()
-
-        annimation_tick_left -= 1
-
-    if len(battle_ground) == 2 and annimation_tick_left == 0:
-        annimation_tick_left = 10
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
